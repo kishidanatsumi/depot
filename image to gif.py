d5328031a6c505888c2b972706a50ccdb4e92ab1
kishidanatsumi <93307938+kishidanatsumi@users.noreply.github.com>
2023-01-19 23:38:42 +0800

    Add files via upload
    UV处理相关

---
+++ image to gif.py
@@ -0,0 +1,13 @@
+import imageio
+import os
+import sys
+
+pic_path = sys.argv[1]
+
+fps = 25
+
+input_images = [os.path.join(pic_path, f) for f in os.listdir(pic_path) if f.endswith('.jpg') or f.endswith('.png')]
+
+imageio.mimsave('animation.gif', [imageio.imread(f) for f in input_images], fps=fps)
+
+print("done")
