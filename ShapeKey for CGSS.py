738fa947eebe3134f2027da3f79c7eb471bb71c1
kishidanatsumi <93307938+kishidanatsumi@users.noreply.github.com>
2023-01-23 23:34:02 +0800

    Update ShapeKey for CGSS.py

--- ShapeKey for CGSS.py
+++ ShapeKey for CGSS.py
@@ -21,24 +21,27 @@ for act in action:
    bpy.context.object.animation_data.action = bpy.data.actions.get(act.name)
    print("Apply:",act)
    bpy.context.view_layer.objects.active = bpy.context.scene.objects["M_Head"]
-   #fix shape key
    bpy.ops.object.modifier_apply_as_shapekey(keep_modifier = True, modifier = object_sel, report = True)
-   #clear pose
+   
    bpy.context.view_layer.objects.active = bpy.context.scene.objects[object_sel]
    bpy.ops.object.mode_set(mode = 'POSE')
    bpy.ops.pose.select_all(action='SELECT')
    bpy.ops.pose.transforms_clear()
    bpy.ops.object.mode_set(mode = 'OBJECT')
 
-
 #rename
 bpy.context.view_layer.objects.active = bpy.context.scene.objects["M_Head"]
 length=len(action)
-
 for num in range(0,length):
+    
  act_str=str(action[num])
- match=re.search("(face.*?)\|", act_str)
- result=match.group(1)
+ matched =re.search("(face.*?)\|", act_str)
+
+ #exceptions if action name do not contain "|"
+ if not matched:
+    continue
+ 
+ result=matched.group(1)
  bpy.context.object.active_shape_key_index = num + 1
  bpy.context.object.active_shape_key.name = result
  
