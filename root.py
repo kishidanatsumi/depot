1b22d50627cf98abd5f53d94abf3290e2a6deb62
kishidanatsumi <93307938+kishidanatsumi@users.noreply.github.com>
2024-03-11 23:28:40 +0800

    Update root.py

--- root.py
+++ root.py
@@ -15,8 +15,7 @@ time_z = [entry["time"] for entry in z_value]
 array_z = [entry["value"] for entry in z_value]
 print("time_x:",len(time_x),"value_x:",len(array_x),"\ntime_z:",len(time_z),"value_z:",len(array_z))
 
-
-i = 0
+i = 1
 z_pointer = 0
 
 #fix
@@ -31,15 +30,15 @@ while (i < len(time_x)):
         #print (time_x[i],time_z[i - z_pointer])
         i=i+1
 
-i = 0
+i = 1
 
 with open(os.path.basename(input_json)+".txt", "w",encoding='utf-8') as outfile:
     outfile.write('version:,2\n')
     outfile.write('modelname:,foobar\n')
-    outfile.write('boneframe_ct:,'+str(len(time_x))+'\n')
+    outfile.write('boneframe_ct:,'+str(len(time_x)-1)+'\n')
     outfile.write('bone_name,frame_num,Xpos,Ypos,Zpos,Xrot,Yrot,Zrot,phys_disable,interp_x_ax,interp_x_ay,interp_x_bx,interp_x_by,interp_y_ax,interp_y_ay,interp_y_bx,interp_y_by,interp_z_ax,interp_z_ay,interp_z_bx,interp_z_by,interp_r_ax,interp_r_ay,interp_r_bx,interp_r_by\n')
     while (i < len(time_x)):
-        outfile.write('センター,'+str(i)+','+str(-(array_x[i])*12)+',0.0,'+str(-(array_z[i])*12)+',0.0,0.0,0.0,False,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127\n')
+        outfile.write('センター,'+str(i-1)+','+str(-(array_x[i])*12)+',0.0,'+str(-(array_z[i])*12)+',0.0,0.0,0.0,False,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127,127\n')
         i=i+1
     outfile.write('morphframe_ct:,0\n')
     outfile.write('camframe_ct:,0\n')
