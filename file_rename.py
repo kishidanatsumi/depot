43533116a71c521aeab81d4291691928873a2f21
kishidanatsumi <93307938+kishidanatsumi@users.noreply.github.com>
2024-03-26 22:42:30 +0800

    Update file_rename.py

--- file_rename.py
+++ file_rename.py
@@ -1,12 +1,12 @@
 import os
 #快速文件替换/重命名
-
+name="foo"
 def file_rename():
     current_dir = os.getcwd()
     files = os.listdir(current_dir)
     for file in files:
-        if '[1]' in file:
-            new_filename = file.replace('[1]', '')
+        if name in file:
+            new_filename = file.replace(name, '')
             os.rename(file, new_filename)
             
 file_rename()
