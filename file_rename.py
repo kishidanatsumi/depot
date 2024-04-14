f25b8e73f633e18db99440325f1154ad953eadad
kishidanatsumi <93307938+kishidanatsumi@users.noreply.github.com>
2024-04-14 13:16:06 +0800

    Update file_rename.py

--- file_rename.py
+++ file_rename.py
@@ -1,13 +1,13 @@
 import os
 import re
 #快速文件替换重命名
-name="foo"
+name="^.* - "
 def file_rename():
     current_dir = os.getcwd()
     files = os.listdir(current_dir)
     for file in files:
-        if re.search("^.* - ",file):
-            ret=re.search(".* - ",file)
+        if re.search(name,file):
+            ret=re.search(name,file)
             new_filename = file.replace(str(ret.group()),"")
             print(file,"has been renamed to",new_filename)
             #os.rename(file, new_filename)
