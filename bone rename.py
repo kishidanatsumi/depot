ddc80359e891d791304a6f571dddff201bc3922b
kishidanatsumi <93307938+kishidanatsumi@users.noreply.github.com>
2024-10-08 23:42:36 +0800

    Update bone rename.py

--- bone rename.py
+++ bone rename.py
@@ -3,7 +3,7 @@ import re
 import datetime
 print("=== Start at",datetime.datetime.now().strftime('%X'),"===")
 
-#1:CGSS 2:CGSS motion 3:SIFAS 4:MLTD 5:SCSP 6:SCSP motion 7:starlit season motion
+#1:CGSS 2:CGSS motion 3:SIFAS 4:MLTD 5:SCSP 6:SCSP motion 7:starlit season motion 8;umamusume
 dic_sel = 5
 
 dic_cgss = {
@@ -51,6 +51,30 @@ dic_cgss_mot = {
 "Middle_01_R":"右中指１","Middle_02_R":"右中指２","Middle_03_R":"右中指３",
 "Ring_01_R":"右薬指１","Ring_02_R":"右薬指２","Ring_03_R":"右薬指３",
 "Pinky_01_R":"右小指１","Pinky_02_R":"右小指２","Pinky_03_R":"右小指３"
+}
+
+dic_uma = {
+"Position":"全ての親",
+"Root":"上半身0","Neck":"首","Head":"頭","Waist":"上半身1","Chest":"上半身2",
+
+"Shoulder_L":"左肩","Arm_L":"左腕","Elbow_L":"左ひじ","Wrist_L":"左手首",
+"Thigh_L":"左足","Knee_L":"左ひざ","Ankle_L":"左足首","Toe_L":"左つま先",
+
+"Thumb_01_L":"左親指０","Thumb_02_L":"左親指１","Thumb_03_L":"左親指２",
+"Index_01_L":"左人指１","Index_02_L":"左人指２","Index_03_L":"左人指３",
+"Middle_01_L":"左中指１","Middle_02_L":"左中指２","Middle_03_L":"左中指３",
+"Ring_01_L":"左薬指１","Ring_02_L":"左薬指２","Ring_03_L":"左薬指３",
+"Pinky_01_L":"左小指１","Pinky_02_L":"左小指２","Pinky_03_L":"左小指３",
+
+"Shoulder_R":"右肩","Arm_R":"右腕","Elbow_R":"右ひじ","Wrist_R":"右手首",
+"Thigh_R":"右足","Knee_R":"右ひざ","Ankle_R":"右足首","Toe_R":"右つま先",
+
+"Thumb_01_R":"右親指０","Thumb_02_R":"右親指１","Thumb_03_R":"右親指２",
+"Index_01_R":"右人指１","Index_02_R":"右人指２","Index_03_R":"右人指３",
+"Middle_01_R":"右中指１","Middle_02_R":"右中指２","Middle_03_R":"右中指３",
+"Ring_01_R":"右薬指１","Ring_02_R":"右薬指２","Ring_03_R":"右薬指３",
+"Pinky_01_R":"右小指１","Pinky_02_R":"右小指２","Pinky_03_R":"右小指３",
+
 }
 
 dic_sifas = {
@@ -231,7 +255,14 @@ elif dic_sel == 7 :
     dic=dic_starlit
 #    target_obj=bpy.context.active_object
     target_obj=bpy.context.active_object
+
+#马
+elif dic_sel == 8 :
+    dic=dic_uma
+#    target_obj=bpy.context.active_object
+    target_obj=bpy.context.active_object
     
+
 else :
     print("Wrong input.")
     exit
