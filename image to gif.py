7a175665c879a52d3ea3522e5fa3fa5feaf35eeb
kishidanatsumi <93307938+kishidanatsumi@users.noreply.github.com>
2026-06-14 23:33:23 +0800

    Update image to gif.py

--- image to gif.py
+++ image to gif.py
@@ -4,22 +4,35 @@ from PIL import Image
 
 fps = 30
 input_files = sys.argv[1:]
-
-sel = 0
+sel = 1
 
 output_file = "out.gif"
 images = [Image.open(file).convert('RGBA') for file in input_files]
 
 width, height = images[0].size
 
+def contr(x,con):
+    y= x + (x - 127)*con/255
+    return int(y)
+
 if sel == 1:
+    bright=96
+    contrast=100
+    #亮度
     for image in images:
         print("start process:",image)
         for y in range(height):
             for x in range(width):
                 r, g, b, a = image.getpixel((x, y))
-                if a < 128:
-                    image.putpixel((x, y), (0, 0, 0, 0))
+                image.putpixel((x, y), (r+bright, g+bright, b+bright, a))
+
+    #对比度
+    for image in images:
+        print("start process:",image)
+        for y in range(height):
+            for x in range(width):
+                r, g, b, a = image.getpixel((x, y))
+                image.putpixel((x, y), (contr(r,contrast),contr(g,contrast),contr(b,contrast), a))
 
 print("start gif")
 
